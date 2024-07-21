b0ea677b8d32711e59b51cdcae250ca4fce802e2
cleankicks11 <cleankicks11@gmail.com>
2024-07-21 13:47:45 -0700

    lambad expression and functional interface - function to call function - js next

--- ClassBox-Java-Test/Class-OOP-test/Main.java
+++ ClassBox-Java-Test/Class-OOP-test/Main.java
@@ -3,6 +3,19 @@ import java.util.*;
 class Main {
 
   public static void main(String[] args){
+    // lambda expression to create the object
+        new Thread(() -> {
+            System.out.println("New thread in lambda and functional interface");
+        }).start();
+
+    //Lambda expression example
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(5);
+    numbers.add(9);
+    numbers.add(8);
+    numbers.add(1);
+    numbers.forEach( (n) -> { System.out.println("Lambda expression collection: "+n); } );
+
     //Instatiate child class in project folder
     try{
       ChildClass child = new ChildClass(1, "ChildClass", true);
